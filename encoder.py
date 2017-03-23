52c87391e0dfbd19e877ecad4e9cb2686bec48cd
Alexander Plavin <alexander@plav.in>
2017-03-23 17:01:37 +0300

    support different modes of subchannels selection & add example

--- encoder.py
+++ encoder.py
@@ -4,36 +4,55 @@ from scipy.optimize import brentq
 __all__ = ['PolarBase', 'PolarCodeAWGN', 'PolarCodeBEC', 'PolarCodeBSC']
 
 
-def theta(x):
-    # approximation
-    return np.where(
-        x >= 10,
-        np.sqrt(np.pi / x) * np.exp(-x/4) * (1 - 10 / 7 / x),
-        np.exp(-0.4527 * x**0.86 + 0.0218)
-    )
+class CodeGenEmpirical:
+    # simple empirical method
 
-def theta1(x):
-    # inverse function
-    def _single_val(y):
-        return brentq(lambda t: theta(t) - y, 1e-10, 1e10)
-    return np.vectorize(_single_val)(x)
+    def checknode_mean(self, t):
+        return 2 * t - t**2
+
+    def varnode_mean(self, t):
+        return t**2
+
+
+class CodeGenGaussian:
+    # gaussian approximation
+
+    def theta(self, x):
+        return np.where(
+            x >= 10,
+            np.sqrt(np.pi / x) * np.exp(-x/4) * (1 - 10 / 7 / x),
+            np.exp(-0.4527 * x**0.86 + 0.0218)
+        )
+
+    def theta1(self, x):
+        # inverse function
+        def _single_val(y):
+            return brentq(lambda t: self.theta(t) - y, 1e-10, 1e10)
+        return np.vectorize(_single_val)(x)
+
+    def checknode_mean(self, t):
+        return self.theta1(1 - (1 - self.theta(t))**2)
+
+    def varnode_mean(self, t):
+        return 2 * t
 
 
 class PolarBase:
-    def __init__(self, bhatt_param, N, K, kernel=np.array([[1, 1], [0, 1]])):
+    def __init__(self, bhatt_param, N, K, kernel=np.array([[1, 1], [0, 1]]), mode=CodeGenGaussian()):
         if (np.log(N) / np.log(2)) % 1 != 0:
             raise ValueError('N is not a power of 2')
 
         self.N = N
         self.K = K
         self.kernel = kernel
+        self.mode = mode
 
         n = int(np.log(N) / np.log(2))
         values = np.array([bhatt_param])
 
         for level in range(n):
             values = values[:, np.newaxis]
-            values = np.hstack((theta1(1 - (1 - theta(values))**2), 2 * values)).flatten()
+            values = np.hstack((mode.checknode_mean(values), mode.varnode_mean(values))).flatten()
 
         self.indices = np.argsort(values)[:K]
         self.indices = np.array(list(map(lambda x: int(bin(x)[2:].zfill(n)[::-1], 2), self.indices)))
@@ -52,17 +71,17 @@ class PolarBase:
 
 
 class PolarCodeAWGN(PolarBase):
-    def __init__(self, Ec, N0, N, K):
+    def __init__(self, Ec, N0, N, K, **kwargs):
         bhatt_param = np.exp(-Ec / N0)
-        super(PolarCodeAWGN, self).__init__(bhatt_param, N, K, kernel)
+        super(PolarCodeAWGN, self).__init__(bhatt_param, N, K, **kwargs)
 
 
 class PolarCodeBEC(PolarBase):
-    def __init__(self, epsilon, N, K):
-        super(PolarCodeBEC, self).__init__(epsilon, N, K, kernel)
+    def __init__(self, epsilon, N, K, **kwargs):
+        super(PolarCodeBEC, self).__init__(epsilon, N, K, **kwargs)
 
 
 class PolarCodeBSC(PolarBase):
-    def __init__(self, p, N, K):
+    def __init__(self, p, N, K, **kwargs):
         bhatt_param = (p * (1 - p)) ** 0.5
-        super(PolarCodeBSC, self).__init__(bhatt_param, N, K, kernel)
+        super(PolarCodeBSC, self).__init__(bhatt_param, N, K, **kwargs)
